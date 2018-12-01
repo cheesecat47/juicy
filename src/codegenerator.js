886c40b6fab3a079a580634baf4bbcab540e9739
atg0831 <44899448+atg0831@users.noreply.github.com>
2018-12-01 16:34:52 +0900

    Update codegenerator.js

--- src/codegenerator.js
+++ src/codegenerator.js
@@ -43,6 +43,39 @@ var musicchart=[
   'https://www.youtube.com/watch?v=_qmcg4vqmVg','https://shared-comic.pstatic.net/thumb/webtoon/183559/thumbnail/title_thumbnail_20160516123017_t83x90.jpg','밤에 듣기 좋은 감성 발라드 베스트 24곡 [가사첨부]'
 
 ]
+
+
+var bookchart=[
+  '','http://image.kyobobook.co.kr/images/book/large/282/l9788965402282.jpg','인공지능을 위한 수학 머신러닝',
+  '','http://image.kyobobook.co.kr/images/book/large/979/l9791160505979.jpg','케라스 창시자에게 배우는 딥러닝(Deep Learning with Python) ',
+  '','http://image.kyobobook.co.kr/images/book/large/636/l9788968484636.jpg','밑바닥부터 시작하는 딥러닝',
+  '','http://image.kyobobook.co.kr/images/book/large/925/l9791188612925.jpg','블록체인 무엇인가?',
+  '','http://image.kyobobook.co.kr/images/book/large/422/l9791188621422.jpg','심층 학습(제이펍의 인공지능 시리즈 13)',
+  '','http://image.kyobobook.co.kr/images/book/large/453/l9791188621453.jpg','하이퍼레저 패브릭으로 배우는 블록체인 ',
+  '','http://image.kyobobook.co.kr/images/book/large/255/l9791188621255.jpg','패턴 인식과 머신 러닝(제이펍의 인공지능 시리즈 11)',
+  '','http://image.kyobobook.co.kr/images/book/large/731/l9791162240731.jpg','핸즈온 머신러닝',
+  '','http://image.kyobobook.co.kr/images/book/large/232/l9791158391232.jpg','텐서플로 딥러닝 프로젝트(실전활용!)(위키북스 데이터 사이언스 시리즈 24)',
+  '','http://image.kyobobook.co.kr/images/book/large/546/l9788966260546.jpg','알고리즘 문제 해결 전략 세트(프로그래밍 대회에서 배우는)(전2권) 프로그래밍 대회에서 배우는『알고리즘 문제 해결 전략 세트』',
+  '','http://image.kyobobook.co.kr/images/book/large/441/l9791162240441.jpg','처음 배우는 딥러닝 수학',
+  '','http://image.kyobobook.co.kr/images/book/large/006/l9791161752006.jpg','컴퓨터 비전과 딥러닝'
+
+]
+
+var newschart=[
+  'http://it.chosun.com/site/data/html_dir/2018/11/30/2018113002299.html','http://itimg.chosun.com/sitedata/itchosun/section_image/20181201114059547_1.jpg','51년 장수 괴도 만화·애니 루팡3세 우리가 흔히 말하는 오덕(Otaku)은 해당 분야를 잘 아는 마니아를 뜻함과 동시에 팬덤 등 열정을 상징하는 말로도 통합니다. IT조선은 2018년 시작과 함께 애니메이션・만화・영화・게임 등 오덕 문화로 상징되는 팝컬처(Pop Culture) 콘텐츠에 대한 이야기를 풀어가고자 합니다. 어린시절..',
+  'http://it.chosun.com/site/data/html_dir/2018/12/01/2018120100013.html','http://itimg.chosun.com/sitedata/itchosun/section_image/20181201092135860_1.jpg','메탈빌드 그레이트 마징가 등 5선IT조선은 매주 토요일 한 주간 예약판매가 시작된 피규어・프라모델 상품 중 팝컬쳐 마니아의 눈길을 끌기 충분한 모형 신상품 다섯 개를 엄선해 소개합니다.',
+  'http://it.chosun.com/site/data/html_dir/2018/11/20/2018112001016.html','http://itimg.chosun.com/sitedata/itchosun/section_image/20181120115654701_1.jpg','[알림] IT조선이 라이브 포토 리포터를 모집합니다조선미디어 그룹의 ICT 전문 매체인 IT조선이 ‘오늘과 지금’을 생생하게 전달할 ‘투데이나우(TodayNow)’의 라이브 포토 리포터를 모집합니다.',
+  'http://it.chosun.com/site/data/html_dir/2018/11/30/2018113002567.html','http://itimg.chosun.com/sitedata/itchosun/section_image/20181201092153898_1.jpg','[이번 주 신상품] 시린랩스 피네이·갤럭시노트9 알파인 화이트IT조선을 통해 11월 26일부터 11월 30일까지 소개된 신상품을 ‘이번 주 신상품’으로 묶어 소개합니다.◇ 시린랩스 피네이',
+  'http://it.chosun.com/site/data/html_dir/2018/11/30/2018113002637.html','http://itimg.chosun.com/sitedata/itchosun/section_image/20181130204500851_1.jpg','정부, 클라우드 장애 아마존 조사 착수정부가 22일 클라우드 접속 장애를 빚은 아마존웹서비스(AWS)에 대한 조사에 착수한다. AWS 한국법인은 26일 조사 권한을 가진 과학기술정보통신부(이하 과기정통부) 산하 중앙전파관리소로부터 다음주 중 조사를 하겠다는 통보를 받은 것으로 알려졌다.이는 AWS 서비스 장애, 이에 다른 이용자 고지, 피해..',
+  'http://it.chosun.com/site/data/html_dir/2018/11/30/2018113002604.html','http://itimg.chosun.com/sitedata/itchosun/section_image/20181130204422442_1.png','독일, 시장지배력 남용 혐의로 아마존 조사 독일 반독점당국이 아마존의 시장지배력 남용 혐의에 대해 조사에 들어갔다. 아마존에 있어 독일은 전 세계에서 두 번째로 큰 시장이다. 29일(현지시각) 영국 파이낸셜타임스(FT)에 따르면 독일 연방카르텔청의 안드레아스 문트 청장은 "아마존은 독일에서 가장 큰 온라인 소매업자인 동시에 가장 큰 시장 역할을 ..',
+  'http://it.chosun.com/site/data/html_dir/2018/11/30/2018113002594.html','http://itimg.chosun.com/sitedata/itchosun/section_image/20181130204350247_1.jpg','구광모 LG 회장 등 상속세 9215억원 신고 구광모 LG그룹 회장을 비롯한 상속인이 9000억원대 상속세를 신고하고, 이중 1차 상속세액 납부를 완료한 것으로 30일 확인됐다.지주사 LG그룹 한 관계자는 "구 회장 등 상속인이 고(故) 구본무 회장의 LG 주식에 대한 상속세 9215억원 등을 과세 당국에 신고했다"며 "연부연납 제도를 통해 향후 최..',
+  'http://it.chosun.com/site/data/html_dir/2018/11/30/2018113002569.html','http://itimg.chosun.com/sitedata/itchosun/section_image/20181130204203265_1.jpg','이통3사, 12월 1일 5G 전파 발사 과학기술정보통신부(이하 과기정통)는 30일 이통3사가 2018년도분 주파수 할당 대가를 납부함에 따라 주파수 할당 통지서를 배부했다고 밝혔다.이통3사는 5G 상용화를 위한 정부의 행정적 절차가 모두 끝냈고, 12월 1일 0시 5G 서비스를 개시한다.',
+  'http://it.chosun.com/site/data/html_dir/2018/11/30/2018113002369.html','http://itimg.chosun.com/sitedata/itchosun/section_image/20181130204014283_1.jpg','삼성, 반도체 투자 줄이고도 점유율 21% 1위 삼성전자가 올해 전 세계 반도체 업체 중 가장 많은 시설투자액을 집행할 것으로 조사됐다. SK하이닉스도 1년새 설비투자를 가장 많이 늘린 업체로 등극했다.30일 시장조사업체 IC인사이츠에 따르면, 2018년 전 세계 반도체 업체의 시설투자 규모는 총 1071억4000만달러(120조1580억원)로 추산된다..',
+  'http://it.chosun.com/site/data/html_dir/2018/11/30/2018113002294.html','http://itimg.chosun.com/sitedata/itchosun/section_image/20181130174645930_1.jpg','AWS, 머신 러닝 기반 차세대 AI 기능·서비스 13종 공개아마존웹서비스(Amazon Web Services, 이하 AWS)가 26일부터 30일(현지시각)까지 미국 라스베이거스에서 열리는 연례 기술 콘퍼런스 ‘AWS 리인벤트(re:Invent) 2018’에서 자사의 새로운 머신 러닝 기반 기능 및 서비스 13종을 대거 공개했다.'
+  //'https://movie.naver.com/movie/bi/mi/basic.nhn?code=170879','https://movie-phinf.pstatic.net/20181002_242/1538464541134fcd6K_JPEG/movie_image.jpg?type=m99_141_2','할로윈']
+
+]
+
 var makecode={
   maketab:function(currentpage){
     var dir;              // 경로 나타낼 변수
@@ -209,6 +242,34 @@ document.write(`
   `);
   i+=3;
   if(i==36)break;
+}
+}
+  ,
+makechartbook:function(){
+  var i=0;
+  while(1){
+  document.write(`
+
+      <img src="${bookchart[i+1]}" alt="${bookchart[i+2]}">
+      <h3 class="book">${bookchart[i+2]}</h3>
+    `);
+    i+=3;
+    if(i==36)break;
+
+}
+}
+,
+makechartnews:function(){
+  var i=0;
+  while(1){
+document.write(`
+    <a href="${newschart[i]}" class="filmchart_grid_item" style="text-decoration:none;">
+    <img src="${newschart[i+1]}" alt="${newschart[i+2]}">
+    <h3 class="news">${newschart[i+2]}</h3>
+  </a>
+  `);
+  i+=3;
+  if(i==30)break;
 }
 }
 }//end makecode
